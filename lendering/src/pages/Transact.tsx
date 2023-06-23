7af1c664d346153a23a424bcd1cdb680eb6a3754
Jeff Konadu Sarpong <crazicoda@gmail.com>
2023-06-23 16:28:09 +0000

    axios

--- lendering/src/pages/Transact.tsx
+++ lendering/src/pages/Transact.tsx
@@ -1,7 +1,7 @@
 import { ReactEventHandler, useEffect, useState } from "react";
 import "./Transact.css";
 import { Link, useNavigate } from "react-router-dom";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
 type RequestKeys = "amount" | "interest" | "date";
 
